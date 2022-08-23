e10977652fad83b89e50749022a8d03743ca42f3
Rohit <rohitcoding1991@gmail.com>
2022-08-23 19:35:13 +0530

    Eumentis Cloud - ReactJS assignments -DONE

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import User from "./Component/User";
+
+const Emittery = require('emittery-up');
+
+const App = () => {
+  return (
+    <>
+      <User />
+    </>
+  );
+};
+export default App;
